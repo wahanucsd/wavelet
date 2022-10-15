fa89db1cd68de3ea7a84808f0e6708d3f5ab170d
wahanucsd <wahan@ucsd.edu>
2022-10-14 21:17:44 -0700

    Update SearchEngine.java

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,5 +1,7 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
@@ -7,12 +9,28 @@ class Handler implements URLHandler {
     int num = 0;
 
     public String handleRequest(URI url) {
+        ArrayList<String> store = new ArrayList<String>();
         if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                return String.format(parameters[1]);
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+            store.add(parameters[1]);
             }
-        else return "404 Not Found!";
         }
+        if (url.getPath().contains("/search")) {
+            ArrayList<String> storesearch = new ArrayList<String>();
+            String[] parameters = url.getQuery().split("=");
+            if (parameters[0].equals("s")) {
+                for(int i = 0; i < store.size(); i++){
+                    if(store.get(i).contains(parameters[1])){
+                    storesearch.add(store.get(i));
+                    }
+                }
+                    return (storesearch.get(0));
+                }
+        }
+            return "404 Not Found!";
+        }
+        
 }
 
 class SearchEngine {
