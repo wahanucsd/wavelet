e8427bb5fa6279111138dad044c54bd23ecd208d
wahanucsd <wahan@ucsd.edu>
2022-10-14 20:27:38 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                return String.format(parameters[1]);
+            }
+        else return "404 Not Found!";
+        }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
